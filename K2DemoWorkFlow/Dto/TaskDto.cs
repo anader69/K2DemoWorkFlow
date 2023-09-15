508071506c5fb15d816ec789b77ac342728476e2
aebaid <aebaid@AEBAID-LPT>
2023-09-15 18:21:42 +0300

    get tasks

---
+++ K2DemoWorkFlow/Dto/TaskDto.cs
@@ -0,0 +1,21 @@
+﻿
+using Framework.Core.Data;
+using K2DemoWorkFlow.Domain.Enum;
+
+namespace K2DemoWorkFlow.Domain.Entities.Workflow
+{
+    public class TaskDto  
+    {  
+         public Guid Id { get; set; }
+         public int? ProcessInstanceId { get; set; }
+        public string ProcessName { get; set; }
+        public string TaskStatusNameAr { get; set; }
+        public string TaskStatusNameEn { get; set; }
+         public DateTime? TaskDate { get; set; }
+        public string Originator { get; set; }
+        public string AssignedTo { get; set; }
+        public string AssignedToFullName { get; set; }
+        public string ProcessActivityAr { get; set; }
+        public string ProcessActivityEn { get; set; }
+     }
+}
